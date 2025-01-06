428250d43482b85074dc0987c2241f3185b7c282
ElSnaps <sunnyblakewebber@gmail.com>
2025-01-06 14:41:07 +0000

    Improved Remove Intermediate ToolScript

--- RemoveProjectIntermediate.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-PowerShell.exe -NoProfile -NoLogo -ExecutionPolicy Bypass -Command "& '%~dp0/Tools/Scripts/RemoveProjectIntermediate.ps1'"
-pause
--- Tools/Scripts/RemoveProjectIntermediate.ps1
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2025 Daft Software.
-
-. $PSScriptRoot/SetupEnv.ps1
-
-Remove-Item -Path "$projectRoot/Intermediate/Build/Win64" -Recurse -Force
-write-host "Cleaned Build/Win64"
